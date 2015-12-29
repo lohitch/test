b1cbe22c10061a425f0f7800a3678ef067255b46
lohitch <lohitch@gmail.com>
2015-12-29 12:42:04 +0530

    removed splunk_server

--- splunk_server/metadata.rb
+++
@@ -1,7 +0,0 @@
-name             'splunk_server'
-maintainer       'Opex Software'
-maintainer_email 'lohit.halemani@opexsoftware.com'
-license          'All rights reserved'
-description      'Installs/Configures splunk_server'
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
